11b6b8ee515acfe3f7c47326db0573ff2336e862
Adam Zoltan Nagy <adam.nagy@accolade.com>
2021-07-30 14:14:39 +0200

    generate random cards

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,6 +9,18 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber === 1) {
+        return 11
+    }
+    else if (randomNumber > 10) {
+        return 10
+    }
+    else {
+        return randomNumber
+    }
+}
 
 function startGame() {
     rednerGame()
@@ -34,7 +46,7 @@ function rednerGame() {
 
 
 function newCard() {
-    let card = 6
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     rednerGame()
